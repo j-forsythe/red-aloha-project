62687bfa006240abb06d697679d4944f1578a561
Jillian Forsythe <Jillian Forsythe>
2016-02-15 11:33:10 -0800

    Fix header covering section top

--- js/script.js
+++ js/script.js
@@ -1,45 +1,43 @@
 $(function(){
 
-  // smooth scrolling http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+	// smooth scrolling http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
 	$('a[href^="#"]').on('click',function (event) {
-	    event.preventDefault();
+		event.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+		var target = this.hash;
+		var $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top -93
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
 	});
 
 });
 
 $(function() {
 
-  $('.bxslider').bxSlider({
+	$('.bxslider').bxSlider({
 		speed: 150,
 		controls: false,
 		auto: true,
 		minSlides: 1,
 		maxSlides: 4,
 		slideMargin: 0
-
 	});
-
 });
 
 
 
 $('form').submit(function(event) {
 
-  event.preventDefault();
+	event.preventDefault();
 
-  if($("#email").val() == '') {
-    alert('Please enter valid email');
-  }
-  else {
-    alert('Thanks for subscribing!');
-  }
+	if($("#email").val() == '') {
+		alert('Please enter valid email');
+	}
+	else {
+		alert('Thanks for subscribing!');
+	}
 });
